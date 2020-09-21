7c5699fcbf1224bb942283a507f751bead2e5516
slpsugar <poppadomsorbread@gmail.com>
2020-09-21 22:21:47 +0300

    add js

---
+++ assets/works.js
@@ -0,0 +1,11 @@
+const aleft = document.querySelector (".aleft");
+        
+aleft.onclick=function () {
+	document.querySelector (".grid").scrollLeft -=1500;
+}
+
+const aright = document.querySelector (".aright");
+
+aright.onclick=function () {
+	document.querySelector (".grid").scrollLeft +=1500;
+}
