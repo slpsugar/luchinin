9af18af07fe8fac74f73220796733a903e58cb37
slpsugar <poppadomsorbread@gmail.com>
2020-09-30 01:06:30 +0300

    edit draft

--- assets/works.js
+++ assets/works.js
@@ -1,13 +1,44 @@
-const aleft = document.querySelector (".aleft");
+// const aleft = document.querySelector (".aleft");
         
-aleft.onclick=function () {
-	document.querySelector (".grid").scrollLeft -=1500;
-}
+// aleft.onclick=function () {
+// 	document.querySelector (".grid").scrollLeft -=1500;
+// }
 
-const aright = document.querySelector (".aright");
+// const aright = document.querySelector (".aright");
 
-aright.onclick=function () {
-	document.querySelector (".grid").scrollLeft +=1500;
-}
+// aright.onclick=function () {
+// 	document.querySelector (".grid").scrollLeft +=1500;
+// }
 
+
+var img001 = document.getElementById("img001");
+var row1 = document.getElementById("row-1");
+var init = () => {
+	img001.style.opacity = '0'; //hide by default when page is shown
+
+	row1.addEventListener('mouseover', () => {
+	img001.style.opacity = '1';
+	});
+
+	row1.addEventListener('mouseout', () => {
+	img001.style.opacity = '0.5';
+	});
+};
+init();
+
+
+var img002 = document.getElementById("img002");
+var row2 = document.getElementById("row-2");
+var init = () => {
+	img002.style.opacity = '0'; //hide by default when page is shown
+
+	row2.addEventListener('mouseover', () => {
+	img002.style.opacity = '1';
+	});
+
+	row2.addEventListener('mouseout', () => {
+	img002.style.opacity = '0.5';
+	});
+};
+init();
 
