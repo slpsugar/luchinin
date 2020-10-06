922091e604995e966d4ee0769609ce830e123e42
slpsugar <poppadomsorbread@gmail.com>
2020-10-06 23:26:50 +0300

    add mob

---
+++ assets/index-mob.js
@@ -0,0 +1,14 @@
+
+
+// var contact = document.querySelector('nav3');
+// contact.addEventListener("click", () => {contact.style.color = "black";});
+
+// function show(){
+//     document.querySelectorAll("mob").style.opacity="1";
+// }
+
+// document
+//     .querySelector (".nav3")
+//     .addEventListener ("click", function () {
+//         document.querySelector(".nav5", ".nav6").forEach(function (show) {nav5.classList.add ("show");});
+//     });
