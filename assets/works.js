d095879aab79c92db1ca52a52f96c75d683d6f6f
slpsugar <poppadomsorbread@gmail.com>
2020-10-09 18:29:39 +0300

    edit works.js

--- assets/works.js
+++ assets/works.js
@@ -62,71 +62,91 @@ var full012 = document.getElementById("full012");
 
 var img013 = document.getElementById("img013");
 var row13 = document.getElementById("row-13");
+var full013 = document.getElementById("full013");
 
 var img014 = document.getElementById("img014");
 var row14 = document.getElementById("row-14");
+var full014 = document.getElementById("full014");
 
 var img015 = document.getElementById("img015");
 var row15 = document.getElementById("row-15");
+var full015 = document.getElementById("full015");
 
 var img016 = document.getElementById("img016");
 var row16 = document.getElementById("row-16");
+var full016 = document.getElementById("full016");
 
 var img017 = document.getElementById("img017");
 var row17 = document.getElementById("row-17");
+var full017 = document.getElementById("full017");
 
 var img018 = document.getElementById("img018");
 var row18 = document.getElementById("row-18");
+var full018 = document.getElementById("full018");
 
 var img019 = document.getElementById("img019");
 var row19 = document.getElementById("row-19");
+var full019 = document.getElementById("full019");
 
 var img020 = document.getElementById("img020");
 var row20 = document.getElementById("row-20");
+var full020 = document.getElementById("full020");
 
 var img021 = document.getElementById("img021");
 var row21 = document.getElementById("row-21");
+var full021 = document.getElementById("full021");
 
 var img022 = document.getElementById("img022");
 var row22 = document.getElementById("row-22");
+var full022 = document.getElementById("full022");
 
 var img023 = document.getElementById("img023");
 var row23 = document.getElementById("row-23");
+var full023 = document.getElementById("full023");
 
 var img024 = document.getElementById("img024");
 var row24 = document.getElementById("row-24");
+var full024 = document.getElementById("full024");
 
 var img025 = document.getElementById("img025");
 var row25 = document.getElementById("row-25");
+var full025 = document.getElementById("full025");
 
 var img026 = document.getElementById("img026");
 var row26 = document.getElementById("row-26");
+var full026 = document.getElementById("full026");
 
 var img027 = document.getElementById("img027");
 var row27 = document.getElementById("row-27");
+var full027 = document.getElementById("full027");
 
 var img028 = document.getElementById("img028");
 var row28 = document.getElementById("row-28");
+var full028 = document.getElementById("full028");
 
 var img029 = document.getElementById("img029");
 var row29 = document.getElementById("row-29");
+var full029 = document.getElementById("full029");
 
 var img030 = document.getElementById("img030");
 var row30 = document.getElementById("row-30");
+var full030 = document.getElementById("full030");
 
 var img031 = document.getElementById("img031");
 var row31 = document.getElementById("row-31");
+var full031 = document.getElementById("full031");
 
 var img032 = document.getElementById("img032");
 var row32 = document.getElementById("row-32");
+var full032 = document.getElementById("full032");
 
 var img033 = document.getElementById("img033");
 var row33 = document.getElementById("row-33");
+var full033 = document.getElementById("full033");
 
 var img034 = document.getElementById("img034");
 var row34 = document.getElementById("row-34");
-
-
+var full034 = document.getElementById("full034");
 
 
 var init = () => {
@@ -258,51 +278,83 @@ var init = () => {
 
 	row19.addEventListener('mouseover', () => {img019.style.opacity = '1'; img019.style.zIndex='40';});
 	row19.addEventListener('mouseout', () => {img019.style.opacity = '0.15';});
+	row19.addEventListener('click', () => {full019.style.display = 'flex';});
+	full019.addEventListener('click', () => {full019.style.display = 'none';});
 
 	row20.addEventListener('mouseover', () => {img020.style.opacity = '1'; img020.style.zIndex='40';});
 	row20.addEventListener('mouseout', () => {img020.style.opacity = '0.15';});
+	row20.addEventListener('click', () => {full020.style.display = 'flex';});
+	full020.addEventListener('click', () => {full020.style.display = 'none';});
 
 	row21.addEventListener('mouseover', () => {img021.style.opacity = '1'; img021.style.zIndex='40';});
 	row21.addEventListener('mouseout', () => {img021.style.opacity = '0.15';});
+	row21.addEventListener('click', () => {full021.style.display = 'flex';});
+	full021.addEventListener('click', () => {full021.style.display = 'none';});
 
 	row22.addEventListener('mouseover', () => {img022.style.opacity = '1'; img022.style.zIndex='40';});
 	row22.addEventListener('mouseout', () => {img022.style.opacity = '0.15';});
+	row22.addEventListener('click', () => {full022.style.display = 'flex';});
+	full022.addEventListener('click', () => {full022.style.display = 'none';});
 
 	row23.addEventListener('mouseover', () => {img023.style.opacity = '1'; img023.style.zIndex='40';});
 	row23.addEventListener('mouseout', () => {img023.style.opacity = '0.15';});
+	row23.addEventListener('click', () => {full023.style.display = 'flex';});
+	full023.addEventListener('click', () => {full023.style.display = 'none';});
 	
 	row24.addEventListener('mouseover', () => {img024.style.opacity = '1'; img024.style.zIndex='40';});
 	row24.addEventListener('mouseout', () => {img024.style.opacity = '0.15';});
+	row24.addEventListener('click', () => {full024.style.display = 'flex';});
+	full024.addEventListener('click', () => {full024.style.display = 'none';});
 
 	row25.addEventListener('mouseover', () => {img025.style.opacity = '1'; img025.style.zIndex='40';});
 	row25.addEventListener('mouseout', () => {img025.style.opacity = '0.15';});
+	row25.addEventListener('click', () => {full025.style.display = 'flex';});
+	full025.addEventListener('click', () => {full025.style.display = 'none';});
 
 	row26.addEventListener('mouseover', () => {img026.style.opacity = '1'; img026.style.zIndex='40';});
 	row26.addEventListener('mouseout', () => {img026.style.opacity = '0.15';});
+	row26.addEventListener('click', () => {full026.style.display = 'flex';});
+	full026.addEventListener('click', () => {full026.style.display = 'none';});
 
 	row27.addEventListener('mouseover', () => {img027.style.opacity = '1'; img027.style.zIndex='40';});
 	row27.addEventListener('mouseout', () => {img027.style.opacity = '0.15';});
+	row27.addEventListener('click', () => {full027.style.display = 'flex';});
+	full027.addEventListener('click', () => {full027.style.display = 'none';});
 
 	row28.addEventListener('mouseover', () => {img028.style.opacity = '1'; img028.style.zIndex='40';});
 	row28.addEventListener('mouseout', () => {img028.style.opacity = '0.15';});
+	row28.addEventListener('click', () => {full028.style.display = 'flex';});
+	full028.addEventListener('click', () => {full028.style.display = 'none';});
 
 	row29.addEventListener('mouseover', () => {img029.style.opacity = '1'; img029.style.zIndex='40';});
 	row29.addEventListener('mouseout', () => {img029.style.opacity = '0.15';});
+	row29.addEventListener('click', () => {full029.style.display = 'flex';});
+	full029.addEventListener('click', () => {full029.style.display = 'none';});
 
 	row30.addEventListener('mouseover', () => {img030.style.opacity = '1'; img030.style.zIndex='40';});
 	row30.addEventListener('mouseout', () => {img030.style.opacity = '0.15';});
+	row30.addEventListener('click', () => {full030.style.display = 'flex';});
+	full030.addEventListener('click', () => {full030.style.display = 'none';});
 
 	row31.addEventListener('mouseover', () => {img031.style.opacity = '1'; img031.style.zIndex='40';});
 	row31.addEventListener('mouseout', () => {img031.style.opacity = '0.15';});
+	row31.addEventListener('click', () => {full031.style.display = 'flex';});
+	full031.addEventListener('click', () => {full031.style.display = 'none';});
 
 	row32.addEventListener('mouseover', () => {img032.style.opacity = '1'; img032.style.zIndex='40';});
 	row32.addEventListener('mouseout', () => {img032.style.opacity = '0.15';});
+	row32.addEventListener('click', () => {full032.style.display = 'flex';});
+	full032.addEventListener('click', () => {full032.style.display = 'none';});
 
 	row33.addEventListener('mouseover', () => {img033.style.opacity = '1'; img033.style.zIndex='40';});
 	row33.addEventListener('mouseout', () => {img033.style.opacity = '0.15';});
+	row33.addEventListener('click', () => {full033.style.display = 'flex';});
+	full033.addEventListener('click', () => {full033.style.display = 'none';});
 
 	row34.addEventListener('mouseover', () => {img034.style.opacity = '1'; img034.style.zIndex='40';});
 	row34.addEventListener('mouseout', () => {img034.style.opacity = '0.15';});
+	row34.addEventListener('click', () => {full034.style.display = 'flex';});
+	full0348.addEventListener('click', () => {full034.style.display = 'none';});
 };
 init();
 
