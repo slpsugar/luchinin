27e573357d4747e023f4f137383542847c5c7788
slpsugar <poppadomsorbread@gmail.com>
2020-10-12 21:13:36 +0300

    add menu mob index

--- assets/index-mob.js
+++ assets/index-mob.js
@@ -1,14 +1,3 @@
-
-
-// var contact = document.querySelector('nav3');
-// contact.addEventListener("click", () => {contact.style.color = "black";});
-
-// function show(){
-//     document.querySelectorAll("mob").style.opacity="1";
-// }
-
-// document
-//     .querySelector (".nav3")
-//     .addEventListener ("click", function () {
-//         document.querySelector(".nav5", ".nav6").forEach(function (show) {nav5.classList.add ("show");});
-//     });
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
