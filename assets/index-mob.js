e02304510b20772807c3afdf970bb3fffcc96aa7
slpsugar <poppadomsorbread@gmail.com>
2020-10-12 23:24:00 +0300

    edit mob index - menu dropdown - no luck

--- assets/index-mob.js
+++ assets/index-mob.js
@@ -1,5 +1,19 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-    document.getElementById ("start").style.color="black";
-
   }
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+  
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
