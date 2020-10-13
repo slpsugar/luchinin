3bf80961493311692c6cdcd960e6125d42999472
slpsugar <poppadomsorbread@gmail.com>
2020-10-13 23:36:55 +0300

    edit properties nav-mob on index.html

--- assets/index-mob.js
+++ assets/index-mob.js
@@ -1,5 +1,6 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-    document.getElementById("hide1").classList.toggle("hide");
-    document.getElementById("hide2").classList.toggle("hide");
+    document.getElementById("changecolour").classList.toggle("colour");
+    document.getElementById("hide1").classList.toggle("hide1");
+    document.getElementById("hide2").classList.toggle("hide2");
   }
