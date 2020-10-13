e137eef8a63ebc190e282eccde4ae5a3d6ec79c6
slpsugar <poppadomsorbread@gmail.com>
2020-10-13 23:19:14 +0300

    edit index.html nav-mob

--- assets/index-mob.js
+++ assets/index-mob.js
@@ -1,3 +1,5 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("hide1").classList.toggle("hide");
+    document.getElementById("hide2").classList.toggle("hide");
   }
