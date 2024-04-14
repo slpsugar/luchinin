58af348e63f1c1bcab77a91687bbbc82ae16274a
slpsugar <poppadomsorbread@gmail.com>
2024-04-14 12:23:47 +0200

    UPD works and exhibitions 2023 (REED, LIBRARY)

--- assets/slider.js
+++ assets/slider.js
@@ -11,12 +11,16 @@ function fn(e) {
     }
 }
 
-const slideIndex = [1,1,1,1];
-const collection = ["sport", "model", "gift", "longart"]
+const slideIndex = [1,1,1,1,1,1,1,1];
+const collection = ["sport", "model", "gift", "longart", "communal", "prosvet","neighbours", "reed"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
